67c6b5f9b14c764a8f50a7b687dd6dd240866204
peshev <peshev@gmail.com>
2025-01-19 13:04:13 +0200

    Reverse motor direction

--- src/Nuvoton/WheelsonMotor.cpp
+++ src/Nuvoton/WheelsonMotor.cpp
@@ -7,6 +7,7 @@ WheelsonMotor::WheelsonMotor() : i2c(Nuvo.getI2C()){
 
 void WheelsonMotor::setMotor(uint8_t id, int8_t intensity){
 	if(id > 3) return;
+	intensity = -intensity;
 	if(state[id] == intensity) return;
 	if(intensity <= -128){
 		intensity = -127;
@@ -23,7 +24,7 @@ int8_t WheelsonMotor::getMotor(uint8_t id){
 	uint8_t msg[] = { MOTOR_GET_BYTE, id };
 	i2c.request(msg, 2, &state[id], 1);
 
-	return (state[id] * 255 / (int16_t) Settings.get().speedMultiplier);
+	return -(state[id] * 255 / (int16_t) Settings.get().speedMultiplier);
 }
 
 void WheelsonMotor::stopAll(){
